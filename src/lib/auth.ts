80e4c9e6572fc563d9add8bf1fd33cb845b65f27
Ohin Taylor <kazani@kazani.dev>
2025-04-12 22:29:46 -0500

    fix: try to fix CF Pages again

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,30 +1,33 @@
 import {goto} from '$app/navigation';
 import {AtprotoDohHandleResolver, BrowserOAuthClient, OAuthSession} from '@atproto/oauth-client-browser';
 
-export const client = await BrowserOAuthClient.load({
-  clientId: 'https://rolulo.pages.dev/atproto-metadata.json',
-  handleResolver: new AtprotoDohHandleResolver(
-      {dohEndpoint: 'https://cloudflare-dns.com/dns-query'})
-});
-
-client.addEventListener(
-    'deleted',
-    (
-        event: CustomEvent<{sub: string}>,
-        ) => {
-      goto('/');
-    },
-);
-
-const result = await client.init();
-
 export let session: OAuthSession|undefined;
+let client: BrowserOAuthClient;
 
-session = result?.session;
+(async () => {
+  client = await BrowserOAuthClient.load({
+    clientId: 'https://rolulo.pages.dev/atproto-metadata.json',
+    handleResolver: new AtprotoDohHandleResolver(
+        {dohEndpoint: 'https://cloudflare-dns.com/dns-query'})
+  });
 
-if (result) {
-  console.log(`${result.session.sub} was restored (last active session)`);
-}
+  client.addEventListener(
+      'deleted',
+      (
+          event: CustomEvent<{sub: string}>,
+          ) => {
+        goto('/');
+      },
+  );
+
+  const result = await client.init();
+
+  session = result?.session;
+
+  if (result) {
+    console.log(`${result.session.sub} was restored (last active session)`);
+  }
+})()
 
 export async function login(handle: string) {
   try {
